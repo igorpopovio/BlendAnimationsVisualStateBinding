17a635a1f81913223fa7595675450b7b0c2c2801
Igor Popov <igor@igorpopov.io>
2018-06-11 19:59:13 +0300

    Add unit tests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ Tests/MainViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using BlendAnimationsVisualStateBinding2;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        [TestCase(CardinalPoint.North)]
+        [TestCase(CardinalPoint.East)]
+        [TestCase(CardinalPoint.South)]
+        [TestCase(CardinalPoint.Weast)]
+        public void CardinalPointIsUpdatedWhenCommandIsInvokedWith(CardinalPoint cardinalPoint)
+        {
+            var viewModel = new MainViewModel();
+
+            viewModel.SwitchCommand.Execute(cardinalPoint);
+
+            Assert.That(viewModel.CardinalPoint, Is.EqualTo(cardinalPoint));
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efbfe20d-1892-4bac-a9ce-c3d5cf30bc50")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
